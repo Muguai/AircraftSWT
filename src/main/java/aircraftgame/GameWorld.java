5436ce1ada937b275bd4f976c260657894dba74e
Nathalie Levenfalk <84509527+nathalielevenfalk@users.noreply.github.com>
2023-12-07 11:43:55 +0100

    trying to fix image

--- src/main/java/aircraftgame/GameWorld.java
+++ src/main/java/aircraftgame/GameWorld.java
@@ -1,20 +1,25 @@
 package aircraftgame;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class GameWorld {
 	
 	Image mapImage;
+	Canvas canvas;
 
 	public GameWorld(Display display, Shell shell) {
 		try {
-			mapImage = new Image(display, "");
+			mapImage = new Image(display, "C:\\Users\\NLeven\\OneDrive - ManpowerGroup\\Documents\\GitHub\\AircraftSWT\\src\\main\\java\\aircraftgame\\pictest.jpg");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		
+		canvas = new Canvas(shell, SWT.DOUBLE_BUFFERED);
+		//canvas.addKeyListener();
 	}
 	
 	
