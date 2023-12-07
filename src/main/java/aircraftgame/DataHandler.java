c0d569626af759979f5b474e895b1e4db79d187f
Nathalie Levenfalk <84509527+nathalielevenfalk@users.noreply.github.com>
2023-12-07 11:01:35 +0100

    feat: initial DataHandler

---
+++ src/main/java/aircraftgame/DataHandler.java
@@ -0,0 +1,17 @@
+package aircraftgame;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataHandler {
+	
+	private List<GameObject> gameObjects;
+	
+	public DataHandler() {
+		gameObjects = new ArrayList<>();
+	}
+	
+	public void addGameObject(GameObject gameObject) { gameObjects.add(gameObject); }
+	
+	public void removeGameObject(GameObject gameObject) { gameObjects.remove(gameObject); }
+	
+}
