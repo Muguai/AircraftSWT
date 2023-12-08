bdcd012fe039a8000a2917eec96aeddb1c6c0b59
Nathalie Levenfalk <84509527+nathalielevenfalk@users.noreply.github.com>
2023-12-08 09:41:18 +0100

    feat: panable map

--- src/main/java/aircraftgame/GameWorld.java
+++ src/main/java/aircraftgame/GameWorld.java
@@ -16,7 +16,7 @@ public class GameWorld {
 
 	public GameWorld(Display display, Shell shell) {
 		try {
-			mapImage = new Image(display, "C:\\Users\\NLeven\\OneDrive - ManpowerGroup\\Documents\\GitHub\\AircraftSWT\\src\\main\\java\\aircraftgame\\test.jpg");
+			mapImage = new Image(display, "C:\\Users\\NLeven\\OneDrive - ManpowerGroup\\Documents\\GitHub\\AircraftSWT\\src\\main\\java\\aircraftgame\\map.png"); // C:\\Users\\NLeven\\OneDrive - ManpowerGroup\\Documents\\GitHub\\AircraftSWT\\src\\main\\java\\aircraftgame
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -24,11 +24,27 @@ public class GameWorld {
 		// Paint up map background
 		canvas = new Canvas(shell, SWT.DOUBLE_BUFFERED);
 		
+		canvas.setSize(600, 400);
+		
         canvas.addPaintListener(e -> {
-            GC gc = e.gc;
-            gc.drawImage(mapImage, offsetX, offsetY);
+        	GC gc = e.gc;
+        	
+        	// Calculate the destination rectangle within the canvas
+            int destX = 0; // X for top left corner
+            int destY = 0; // Y for top left corner
+            int destWidth = canvas.getBounds().width; // portion of image drawn on canvas
+            int destHeight = canvas.getBounds().height;// portion of image drawn on canvas
+
+            // Calculate the source rectangle within the image, applying the offset values
+            int srcX = Math.max(0, offsetX);
+            int srcY = Math.max(0, offsetY);
+            int srcWidth = Math.min(mapImage.getBounds().width - srcX, destWidth);
+            int srcHeight = Math.min(mapImage.getBounds().height - srcY, destHeight);
+
+         // Draw the portion of the image on the canvas
+            gc.drawImage(mapImage, srcX, srcY, srcWidth, srcHeight, destX, destY, destWidth, destHeight);
         });
-        
+       
         canvas.setFocus();
         
         // Setting up event listeners
