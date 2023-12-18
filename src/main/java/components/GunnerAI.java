4ac4691386f1b130a4f4f942e4799cce19fc5b0e
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2023-12-18 16:26:23 +0100

    Fix: Detection range seems to work now

--- src/main/java/components/GunnerAI.java
+++ src/main/java/components/GunnerAI.java
@@ -28,9 +28,9 @@ public class GunnerAI {
 				continue;
 
 			float x1 = aircraft.getX(); //+ aircraft.offsets[0];
-			float y1 = aircraft.getX(); //+ aircraft.offsets[1];
- 			float x2 = target.getX() + target.offsets[0];
-			float y2 = target.getY() + target.offsets[1];
+			float y1 = aircraft.getY(); //+ aircraft.offsets[1];
+ 			float x2 = target.getX();
+			float y2 = target.getY();
 			if(target instanceof Player) {
 				x2 += ((Player)target).getDisplay().getBounds().width/2;
 				y2 += ((Player)target).getDisplay().getBounds().height/2;
@@ -38,10 +38,12 @@ public class GunnerAI {
 			
 			float xRes = (float)Math.pow(x1-x2, 2);
 			float yRes = (float)Math.pow(y1-y2, 2);
-			
-			if(Math.sqrt(xRes + yRes) <= detectionRange && totalTime >= timer) {
-				totalTime = 0;
+			float distance = (float)Math.sqrt(xRes + yRes);
+			if(distance <= detectionRange && totalTime >= timer) {
 				if(aircraft.friendly != target.friendly) {
+					totalTime = 0;
+					System.out.println("Aircraft: " + x1 + " " + y1);
+					System.out.println("Target: " + x2 + " " + y2);
 					Bullet bullet = new Bullet(display, aircraft, 0, 0, aircraft.friendly);
 					return bullet;
 				}
