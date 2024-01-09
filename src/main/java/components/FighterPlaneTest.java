9a7d8628fe37b60de07d3dbe4693f66cff401a60
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2024-01-09 10:22:34 +0100

    feature: adding more tests

--- src/main/java/components/FighterPlaneTest.java
+++ src/main/java/components/FighterPlaneTest.java
@@ -6,10 +6,10 @@ import org.eclipse.swt.widgets.Display;
 import org.junit.jupiter.api.Test;
 
 class FighterPlaneTest {
-
+	private final static Display display = new Display();
+	
 	@Test
 	void checkMovement() {
-		Display display = new Display();
 		float x = 0;
 		float y = 0;
 		FighterPlane testPlane = new FighterPlane(display, x, y, 0.0f, true);
@@ -17,4 +17,27 @@ class FighterPlaneTest {
 		assert(testPlane.getX() != 0.0f);
 	}
 
+	@Test
+	void checkSpeedIncrease() {
+		float x = 0;
+		float y = 0;
+		FighterPlane fastPlane = new FighterPlane(display, x, y, 0.0f, true);
+		FighterPlane slowPlane = new FighterPlane(display, x, y, 0.0f, true);
+		fastPlane.increaseSpeed();
+		fastPlane.moveObject(1.0f);
+		slowPlane.moveObject(1.0f);
+		assert(fastPlane.getX() > slowPlane.getX());
+	}
+	
+	@Test
+	void checkSpeedDecrease() {
+		float x = 0;
+		float y = 0;
+		FighterPlane fastPlane = new FighterPlane(display, x, y, 0.0f, true);
+		FighterPlane slowPlane = new FighterPlane(display, x, y, 0.0f, true);
+		slowPlane.decreaseSpeed();
+		fastPlane.moveObject(1.0f);
+		slowPlane.moveObject(1.0f);
+		assert(fastPlane.getX() > slowPlane.getX());
+	}
 }
