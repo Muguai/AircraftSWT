57fd23ba13b76dc95c9dbeb25ea379979fb0dfd5
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2024-01-11 13:40:22 +0100

    feat: Adding a test

--- src/main/java/components/FighterPlaneTest.java
+++ src/main/java/components/FighterPlaneTest.java
@@ -9,7 +9,7 @@ class FighterPlaneTest {
 	private final static Display display = new Display();
 	
 	@Test
-	void checkMovement() {
+	void directionalMovement() {
 		float x = 0;
 		float y = 0;
 		FighterPlane testPlane = new FighterPlane(display, x, y, 0.0f, true);
@@ -18,7 +18,7 @@ class FighterPlaneTest {
 	}
 
 	@Test
-	void checkSpeedIncrease() {
+	void speedIncrease() {
 		float x = 0;
 		float y = 0;
 		FighterPlane fastPlane = new FighterPlane(display, x, y, 0.0f, true);
@@ -30,7 +30,7 @@ class FighterPlaneTest {
 	}
 	
 	@Test
-	void checkSpeedDecrease() {
+	void speedDecrease() {
 		float x = 0;
 		float y = 0;
 		FighterPlane fastPlane = new FighterPlane(display, x, y, 0.0f, true);
@@ -39,5 +39,15 @@ class FighterPlaneTest {
 		fastPlane.moveObject(1.0f);
 		slowPlane.moveObject(1.0f);
 		assert(fastPlane.getX() > slowPlane.getX());
+	}
+	
+	@Test
+	void takeDamage() {
+		float x = 0;
+		float y = 0;
+		FighterPlane plane = new FighterPlane(display, x, y, 0.0f, true);
+		int startHealth = plane.getHealth();
+		plane.takeDamage(50);
+		assert plane.getHealth() != startHealth;
 	}
 }
